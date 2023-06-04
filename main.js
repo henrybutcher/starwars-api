f7c28664e8dbbd2d3a05665c15c7b361c5c5ca79
henrybutcher <butcher301355@gamil.com>
2023-06-04 15:54:43 -0400

    test

---
+++ main.js
@@ -0,0 +1,34 @@
+let searchInput = document.getElementById('search');
+
+document.getElementById('get-character').addEventListener
+('click', function(){
+
+});
+
+document.getElementById('get-all-characters').addEventListener
+('click', function(){
+
+});
+
+searchInput.addEventListener('keyup', function(){
+
+});
+
+function fetchRandomCharacter() {
+    let randomID = Math.floor(Math.random() * 9)+1;
+    
+    fetch(`https://swapi.dev/api/people/${randomID}/`) 
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('character').innerHTML = 
+        `<h2>${data.name}</h2>
+        <p>Height: ${data.height}cm</p>
+        <p>Mass: ${data.mass}kg</p>
+        <p>Hair color: ${data.hair_color}</p>
+        <p>Skin color: ${data.skin_color}</p>
+        <p>Eye color: ${data.eye_color}</p>
+        <p>Birth Year: ${data.birth_year}</p>
+        <p>Gender: ${data.gender}</p>`;
+    })
+    .catch(error => console.log('Error: ', error));
+}
